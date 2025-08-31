0a257d7a32e8ea700d1be7178fa3ca9dd8f1b7e1
vishal-pundhir <vishal.pundhir@spyne.ai>
2025-08-31 17:32:54 +0530

    added correct words count, elapsedTiem and WPM basic

--- pages/type/index.js
+++ pages/type/index.js
@@ -2,8 +2,17 @@ import MainLayout from '@/components/layout/MainLayout'
 import React, { useCallback, useEffect, useState } from 'react'
 
 const Type = () => {
+  //Need to add WPM indicator in info section.
+  //The timer starts when the user starts typing
+  //When a word gets finished (when it gets a ' ' character) -> increment correct word count and 
+  //calculate the time (Date.now()) -> calculate the WPM and update it)
   const [wordCount, setWordCount] = useState(10);
   const [typedText, setTypedText] = useState('');
+  const [correctWordCount, setCorrectWordCount] = useState(0);
+  const [WPM, setWPM] = useState(0);
+  const [startTime, setStartTime] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [isTypingFinished, setIsTypingFinished] = useState(false);
 
   const sentenceConfig = [
     { count: 10, sentence: "The quick brown fox jumps over the lazy dog" },
@@ -13,18 +22,52 @@ const Type = () => {
 
   const sentence = sentenceConfig.find(s => s.count === wordCount).sentence;
 
+  const checkCorrectWords = () => {
+
+    const originalWords = sentence.split(" ");
+    const typedWords = typedText.trim().split(" ");
+    let correct = 0;
+    for (let i = 0; i < typedWords.length; i++) {
+      if (typedWords[i] === originalWords[i])
+        correct++;
+    }
+    return correct;
+  }
+
   const handleKeyDown = useCallback((e) => {
+
+    if (!startTime) setStartTime(performance.now());
+
     setTypedText((prev) => {
       if (e.key === 'Backspace') {
         return prev.slice(0, -1);
-      } 
+      }
       else if (e.key.length === 1) {
         const updatedText = prev + e.key;
         return updatedText;
       }
       return prev;
     });
-  }, [sentence]);
+
+  }, [startTime]);
+
+  useEffect(() => {
+    if (typedText.length > 0) {
+      const correct = checkCorrectWords(typedText);
+      setCorrectWordCount(correct);
+
+      if (typedText.length === sentence.length) {
+        console.log("typing finished");
+        const elapsedTime = startTime ? (performance.now() - startTime) / 1000 : 0; // seconds
+        setElapsedTime(elapsedTime);
+        setIsTypingFinished(true);
+
+        // WPM = (correct words / time in minutes)
+        const minutes = elapsedTime / 60;
+        setWPM(correct / minutes);
+      }
+    }
+  }, [typedText]);
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
@@ -71,9 +114,11 @@ const Type = () => {
         </div>
 
         {/* Info container */}
-        {/* <div className='info-container text-white'>
-          Cursor Index: {cursorIndex}
-        </div> */}
+        <div className='info-container text-white'>
+          <p>Correct Words: {correctWordCount}</p>
+          <p>Elapsed Time: {Math.floor(elapsedTime)}s</p>
+          <p>WPM: {Math.round(WPM)}</p>
+        </div>
       </div>
     </MainLayout>
   );
